09e57ebfb15f530e412ad5c24874ea3a769297ba
Jacques Beaurain <jacques.beaurain@geosoft.com>
2017-10-18 04:47:09 -0400

    Illustrate type hints will be generated

--- test.py
+++ test.py
@@ -43,25 +43,19 @@ class GX3DN:
         self._wrapper = wrapper if wrapper else g_cy.Wrap3DN(0)
 
     @classmethod
-    def null(cls):
+    def null(cls) -> 'GX3DN':
         """
-        null() -> GX3DN:
-        
         A null (undefined) instance of :class:`GX3DN`
         
         :returns: A null :class:`GX3DN`
-        :rtype: :class:`GX3DN`
         """
         return GX3DN()
 
-    def is_null(self):
+    def is_null(self) -> bool:
         """
-        is_null() -> bool:
-        
         Check if the instance of :class:`GX3DN is null (undefined)`
         
         :returns: True if this is a null instance of :class:`GX3DN`, False otherwise.
-        :rtype: bool
         """
         return self._wrapper.handle == 0
 
@@ -69,62 +63,43 @@ class GX3DN:
         return self._wrapper.handle
 
     @classmethod
-    def create(cls):
+    def create(cls) -> 'GX3DN':
         """
         create() -> GX3DN:
 
         Creates a 3DN.
 
         :returns: 3DN Object
-        :rtype: :class:`GX3DN`
 
         versionadded:: 5.1.2
         """
         return GX3DN(g_cy.Wrap3DN.create())
 
-    def get_render_controls(self, p2, p3, p4, p6, p8):
+    def get_render_controls(self, p2: int_ref, p3: int_ref, p4: str_ref, p6: str_ref, p8: str_ref) -> None:
         """
-        get_render_controls((int_ref)p2, (int_ref)p3, (str_ref)p4, (str_ref)p6, (str_ref)p8) -> None:
-
         Get the rendering controls
 
         :param p2: Render Bounding Box (0 or 1)
-        :type p2: :class:`int_ref`
         :param p3: Render Axis (0 or 1)
-        :type p3: :class:`int_ref`
         :param p4: Label for X axis
-        :type p4: :class:`str_ref`
         :param p6: Label for Y axis
-        :type p6: :class:`str_ref`
         :param p8: Label for Z axis
-        :type p8: :class:`str_ref`
         :returns: Nothing
-        :rtype: None
-
 
         versionadded:: 6.3.0
         """
         p2.value, p3.value, p4.value, p6.value, p8.value = self._wrapper.get_render_controls(p2.value, p3.value, p4.value.encode(), p6.value.encode(), p8.value.encode())
 
-    def set_render_controls(self, p2, p3, p4, p6, p8):
+    def set_render_controls(self, p2: int, p3: int, p4: str, p6: str, p8: str) -> None:
         """
-        set_render_controls((int)p2, (int)p3, (str)p4, (str)p6, (str)p8) -> None:
-
         Get the rendering controls
 
         :param p2: Render Bounding Box (0 or 1)
-        :type p2: int
         :param p3: Render Axis (0 or 1)
-        :type p3: int
         :param p4: Label for X axis
-        :type p4: str
         :param p6: Label for Y axis
-        :type p6: str
         :param p8: Label for Z axis
-        :type p8: str
         :returns: Nothing
-        :rtype: None
-
 
         versionadded:: 6.3.0
         """
@@ -151,20 +126,20 @@ class GXVV:
         self._wrapper = wrapper if wrapper else g_cy.WrapVV(0)
 
     @classmethod
-    def create(cls, type, elements):
+    def create(cls, type: int, elements: int):
         return GXVV(g_cy.WrapVV.create(type, elements))
 
 
-    def fill_double(self, value):
+    def fill_double(self, value: float):
         self._wrapper.fill_double(value)
 
-    def get_data(self, start, num_elements, b, type):
+    def get_data(self, start: int, num_elements: int, b: type(bytearray), type: int):
         self._wrapper.get_data(start, num_elements, b, 5)
 
-    def get_data_array(self, start, num_elements, dtype):
+    def get_data_array(self, start: int, num_elements: int, dtype: int):
         return self._wrapper.get_data_array(start, num_elements, 5)
 
-    def get_data_np(self, start, num_elements, dtype):
+    def get_data_np(self, start: int, num_elements: int, dtype: int):
         r, a = self._wrapper.get_data_array(start, num_elements, 5)
         return (r, np.asarray(a))
 
@@ -210,19 +185,19 @@ try:
     #finally:
     #    del dn
     #
-    #with GX3DN.create() as dn:
-    #    print(dn)
-    #
-    #    box = int_ref()
-    #    axis = int_ref()
-    #    xl = str_ref()
-    #    yl = str_ref()
-    #    zl = str_ref()
-    #    dn.get_render_controls(box,axis,xl,yl,zl)
-    #    print("box: {}, axis: {}, xl: {}, yl: {}, zl: {}".format(box.value,axis.value,xl.value,yl.value,zl.value))
-    #    dn.set_render_controls(0,0,"a","v","x")
-    #    dn.get_render_controls(box,axis,xl,yl,zl)
-    #    print("box: {}, axis: {}, xl: {}, yl: {}, zl: {}".format(box.value,axis.value,xl.value,yl.value,zl.value))
+    with GX3DN.create() as dn:
+        print(dn)
+    
+        box = int_ref()
+        axis = int_ref()
+        xl = str_ref()
+        yl = str_ref()
+        zl = str_ref()
+        dn.get_render_controls(box,axis,xl,yl,zl)
+        print("box: {}, axis: {}, xl: {}, yl: {}, zl: {}".format(box.value,axis.value,xl.value,yl.value,zl.value))
+        dn.set_render_controls(0,0,"a","v","x")
+        dn.get_render_controls(box,axis,xl,yl,zl)
+        print("box: {}, axis: {}, xl: {}, yl: {}, zl: {}".format(box.value,axis.value,xl.value,yl.value,zl.value))
     print('final end')
 
 finally:
