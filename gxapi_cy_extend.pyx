7c13fbb51731e54246a06bd604fb29ff8a1d4742
Jacques Beaurain <jacques.beaurain@geosoft.com>
2017-10-24 08:40:31 -0400

    Correct type handling in get_data_array_vv

--- gxapi_cy_extend.pyx
+++ gxapi_cy_extend.pyx
@@ -12,7 +12,7 @@ cdef extern int16_t sGetError_GEO(void*, char*, int32_t, char*, int32_t, int32_t
 from geosoft.gxapi import GXCancel, GXExit, GXAPIError, GXError
 
 cdef void callback_free_data(void *p):
-    print("In callback_free_data")
+    #print("In callback_free_data")
     free(p)
 
 cdef _raise_on_gx_errors(void* p_geo):
@@ -45,11 +45,12 @@ cdef _raise_on_gx_errors(void* p_geo):
 
 
 cdef extern int32_t iGetData_VV(void*, const int32_t* p1, int32_t p2, int32_t p3, void* p4, int32_t p5);
+cdef extern int32_t iSetData_VV(void*, const int32_t* p1, int32_t p2, int32_t p3, const void* p4, int32_t p5);
 
 
 cdef class WrapVVExtra:
     @classmethod
-    def get_data_array_vv(cls, uintptr_t p_geo, int32_t vv_handle, int32_t p2, int32_t p3, int32_t p5):
+    def get_data_array_vv(cls, uintptr_t p_geo, int32_t vv_handle, int32_t start, int32_t elements, int32_t gs_type):
         """
         Type code	C Type	Python Type	Minimum size in bytes	Notes
         'b'	signed char	int	1	 
@@ -72,13 +73,53 @@ cdef class WrapVVExtra:
         cdef array arrp4
         cdef size_t itemsize
         try:
-            itemsize=sizeof(double)
-            ap4 = malloc(p3*itemsize)
-            arrp4 = array(shape=(p3,), itemsize=itemsize, format="d", mode="c", allocate_buffer=False)
+            if gs_type == 0:
+                # GS_BYTE
+                format = 'b'
+                itemsize = 1
+            elif gs_type == 1:
+                #  GS_USHORT
+                format = 'H'
+                itemsize = 2
+            elif gs_type == 2:
+                #  GS_SHORT
+                format = 'h'
+                itemsize = 2
+            elif gs_type == 3:
+                # GS_LONG
+                format = 'i'
+                itemsize = 4
+            elif gs_type == 4:
+                # GS_FLOAT
+                format = 'f'
+                itemsize = 4
+            elif gs_type == 5:
+                # GS_DOUBLE
+                format = 'd'
+                itemsize =  8
+            elif gs_type == 6:
+                # GS_UBYTE
+                format = 'B'
+                itemsize = 1
+            elif gs_type == 7:
+                # GS_ULONG
+                format = 'I'
+                itemsize = 4
+            elif gs_type == 8:
+                # GS_LONG64
+                format = 'q'
+                itemsize = 8
+            elif gs_type == 9:
+                # GS_ULONG64
+                format = 'Q'
+                itemsize = 8
+        
+            ap4 = malloc(elements*itemsize)
+            arrp4 = array(shape=(elements,), itemsize=itemsize, format=format, mode="c", allocate_buffer=False)
             arrp4.data = <char*>ap4
             arrp4.callback_free_data = callback_free_data
             ap4 = NULL
-            _return_val = iGetData_VV(<void*>p_geo, &vv_handle, p2, 5, <void*>arrp4.data, p5)
+            _return_val = iGetData_VV(<void*>p_geo, &vv_handle, start, elements, <void*>arrp4.data, gs_type)
             _raise_on_gx_errors(<void*>p_geo)
             return (_return_val, arrp4)
         finally:
