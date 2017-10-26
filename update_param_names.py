a95f18bf80ae55e054532c3ef1aab941905efdd8
Jacques Beaurain <jacques.beaurain@geosoft.com>
2017-10-26 12:52:43 -0400

    Improve coverage of parameter name extraction. Also replace size_of_param values with correct values by using a lambda with callback to construct the replacement string

--- update_param_names.py
+++ update_param_names.py
@@ -16,9 +16,13 @@ recamel1 = re.compile('(.)([A-Z][a-z]+)')
 recamel2 = re.compile('([a-z0-9])([A-Z])')
 
 def convert_camel_case(name):
-	s1 = recamel1.sub(r'\1_\2', name)
-	s2 = recamel2.sub(r'\1_\2', s1).lower()
-	return s2.replace("3_d", "_3d")
+    name = recamel1.sub(r'\1_\2', name)
+    name = recamel2.sub(r'\1_\2', name).lower()
+    name = name.replace("3_d", "_3d")
+    if name.startswith("_3d"):
+        return "o{}".format(name[1:])
+    else:
+        return name
 
 funclookup = defaultdict(list)
 
@@ -26,6 +30,7 @@ gxx_files = glob.glob('e:/ggit/t/core/**/gxx_*.c', recursive=True)
 gxx_files.extend(glob.glob('e:/ggit/t/core/**/gxx_*.cpp', recursive=True))
 gxx_files.extend(glob.glob('e:/ggit/t/desk/**/gxx_*.c', recursive=True))
 gxx_files.extend(glob.glob('e:/ggit/t/desk/**/gxx_*.cpp', recursive=True))
+
 
 for gxx_file in gxx_files:
     with open(gxx_file, 'r') as f:
@@ -57,13 +62,14 @@ for gxx_file in gxx_files:
 
     for f in funcs:
         params = [p[2:] if len(p) > 2 and (p.startswith("pl") or p.startswith("pd") or p.startswith("pc") or p.startswith("sz")) else p for p in f[1:]]
-        params = [convert_camel_case(p) for p in params]
-        params = ['val' if (p == 'pl' or p == 'pd') and 'val' not in params else p for p in params]
-        params = ['str_val' if (p == 'pc' or p == 'sz' or p == 'str' or p == 'string') and 'str_val' not in params else p for p in params]
-        params = ['_{}'.format(p) if p[0] >= '0' and p[0] <= '9' else p for p in params]
-        params = ['lda' if p == 'lambda' and 'lda' not in params else p for p in params]
+        if len(params) > 0:
+            params = [convert_camel_case(p) for p in params]
+            params = ['val' if (p == 'pl' or p == 'pd') and 'val' not in params else p for p in params]
+            params = ['str_val' if (p == 'pc' or p == 'sz' or p == 'str' or p == 'string') and 'str_val' not in params else p for p in params]
+            params = ['v{}'.format(p) if p[0] >= '0' and p[0] <= '9' else p for p in params]
+            params = ['lda' if p == 'lambda' and 'lda' not in params else p for p in params]
         
-        funclookup[f[0]] = params
+            funclookup[f[0]] = params
 
 
 sources = defaultdict(list)
@@ -76,19 +82,20 @@ for file in spec_files:
     with open(file, 'r') as f:
          sources[gx_cl] = [file, f.read()]
 
+def replace_params(match, params):
+    for i in range(0, len(params)):
+        match = match.replace("'p{}'".format(i + 1), "'{}'".format(params[i]))
+    return match
+
 for func, params in funclookup.items():
     cl = func[func.rfind('_')+1:]
-    if len(params) > 0 and cl in sources.keys():
+    if cl in sources.keys():
         method_re = "(\(\'{}\'[^\[]*?\[.*\s)(.*Parameter\(\')(p{})((?:.*\n)*?(?:.*\),?\n))".format(func, 1)
-        replace_str = r'\1\2{}\4'.format(params[0])
-        group_pos = 5
         for i in range(1, len(params)):
             method_re += "(.*Parameter\(\')(p{})((?:.*\n)*?(?:.*\),?\n))".format(i + 1)
-            replace_str += r'\{}{}\{}'.format(group_pos, params[i], group_pos + 2)
-            group_pos = group_pos + 3
 
         reg_func = re.compile(method_re, re.MULTILINE)
-        sources[cl][1] = reg_func.sub(replace_str, sources[cl][1])
+        sources[cl][1] = reg_func.sub(lambda m: replace_params(m[0], params), sources[cl][1])
 
 for cl, pair in sources.items():
     with open(pair[0], 'w') as f:
