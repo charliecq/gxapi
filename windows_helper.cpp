6578ca82bb95704a75b015a0368e09326197a752
Jacques Beaurain <jacques.beaurain@geosoft.com>
2017-10-28 06:22:03 -0400

    Correct Unicode output to console

--- windows_helper.cpp
+++ windows_helper.cpp
@@ -1,5 +1,7 @@
 #include <Windows.h>
 
+#include <string>
+
 extern "C"
 {
    static HWND hWndConsole = nullptr;
@@ -51,11 +53,40 @@ extern "C"
       }
    }
 
+   void stream_helper_write(DWORD dest_handle, const char *data)
+   {
+      wchar_t*   szwOut;
+      DWORD      dwBytesWritten;
+      DWORD      fdwMode;
+      HANDLE     outHandle = GetStdHandle(dest_handle);
+      
+      int nLengthA = (int)strlen(data);
+      int nLengthW = ::MultiByteToWideChar(CP_UTF8, 0, data, nLengthA, NULL, 0);
+      szwOut = (wchar_t*)malloc((nLengthW + 1)*sizeof(wchar_t));
+      ::MultiByteToWideChar(CP_UTF8, 0, data, nLengthA, szwOut, nLengthW);
+      szwOut[nLengthW] = L'\0';
+      
+      if ((GetFileType(outHandle) & FILE_TYPE_CHAR) &&
+         GetConsoleMode(outHandle, &fdwMode))
+         WriteConsoleW(outHandle, szwOut, (DWORD)wcslen(szwOut),
+            &dwBytesWritten, 0);
+      else
+      {
+         int nOutputCP = GetConsoleOutputCP();
+         int charCount = WideCharToMultiByte(nOutputCP, 0, szwOut, -1, 0,
+            0, 0, 0);
+         char* szaStr = (char*)malloc(charCount);
+         WideCharToMultiByte(nOutputCP, 0, szwOut, -1, szaStr, charCount,
+            0, 0);
+         WriteFile(outHandle, szaStr, charCount - 1, &dwBytesWritten, 0);
+         free(szaStr);
+      }
+      free(szwOut);
+   }
 
    void stdout_stream_helper_write(const char *data)
    {
-      DWORD RetVal;
-      WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), data, (DWORD)strlen(data), &RetVal, NULL);
+      stream_helper_write(STD_OUTPUT_HANDLE, data);
    }
 
    void stdout_stream_helper_flush()
@@ -85,8 +116,7 @@ extern "C"
 
    void stderr_stream_helper_write(const char *data)
    {
-      DWORD RetVal;
-      WriteFile(GetStdHandle(STD_ERROR_HANDLE), data, (DWORD)strlen(data), &RetVal, NULL);
+      stream_helper_write(STD_ERROR_HANDLE, data);
    }
 
    void stderr_stream_helper_flush()
