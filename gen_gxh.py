127509708b36b1eede60ee68807c216917d67b91
Jacques Beaurain <jacques.beaurain@geosoft.com>
2017-12-08 14:04:49 -0500

    Test comment purely to test merging with internal repos (2)

--- gen_gxh.py
+++ gen_gxh.py
@@ -185,6 +185,7 @@ class GXHCodeGenerator(CodeGeneratorBase):
             self._regen_gxh(cl)
 
     def generate(self):
+        #Test comment purely to test merging with internal repos
         gen._regen_version()
         gen._regen_all()
         gen._regen_classes()
