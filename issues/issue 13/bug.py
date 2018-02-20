68cb8f7558f69d143c868aba58f2c9185059c663
Ian MacLeod <ian.macleod@geosoft.com>
2018-02-20 12:54:08 -0500

    Issue #13

---
+++ issues/issue 13/bug.py
@@ -0,0 +1,63 @@
+import numpy as np
+import geosoft.gxpy.gx as gx
+import geosoft.gxapi as gxapi
+import geosoft.gxpy.view as gxv
+import geosoft.gxpy.group as gxg
+import geosoft.gxpy.vv as gxvv
+
+gxc = gx.GXpy()
+
+verts = np.array([[0, 0, 0],
+                  [5, 0, 0],
+                  [5, 5, 0],
+                  [0, 3, 5],
+                  [2.5, 2, 10],
+                  [-3, 6, 8],
+                  [-4, 0, 12]], dtype=np.float64)
+faces = np.array([[0, 1, 2],
+                  [0, 2, 3],
+                  [3, 2, 4],
+                  [1, 2, 4],
+                  [3, 4, 5],
+                  [6, 4, 5]], dtype=np.int32)
+
+with gxv.View_3d.new() as v3d:
+    v3d_file = v3d.file_name
+    with gxg.Draw_3d(v3d, 'Surface') as g:
+        verts = verts[faces].reshape(-1, 3)
+        vx, vy, vz = gxvv.vvset_from_np(verts)
+        vf1, vf2, vf3 = gxvv.vvset_from_np(faces)
+        normals = gxg.vertex_normals_np(faces, verts)
+        nx, ny, nz = gxvv.vvset_from_np(normals[faces].reshape(-1, 3))
+
+        # using a constant color does not complain
+        v3d.gxview.draw_surface_3d_ex('test',
+                                      vx.gxvv, vy.gxvv, vz.gxvv,
+                                      nx.gxvv, ny.gxvv, nz.gxvv,
+                                      gxapi.GXVV.null(), gxg.C_GREY,
+                                      vf1.gxvv, vf2.gxvv, vf3.gxvv,
+                                      v3d.coordinate_system.gxipj)
+
+        # using an array color raises invalid number of colour if we pass colours/vertex
+        color = np.array([gxg.C_GREY for i in range(vx.length)])
+        color_vv = gxvv.GXvv(color, dtype=np.int32)
+        try:
+            v3d.gxview.draw_surface_3d_ex('test2',
+                                          vx.gxvv, vy.gxvv, vz.gxvv,
+                                          nx.gxvv, ny.gxvv, nz.gxvv,
+                                          color_vv.gxvv, gxg.C_GREY,
+                                          vf1.gxvv, vf2.gxvv, vf3.gxvv,
+                                          v3d.coordinate_system.gxipj)
+
+        except gxapi.GXAPIError as e:
+            print(str(e))
+
+            # and if pass colours/face it asserts
+            color = np.array([gxg.C_GREY for i in range(faces.shape[0])])
+            color_vv = gxvv.GXvv(color, dtype=np.int32)
+            v3d.gxview.draw_surface_3d_ex('test2',
+                                          vx.gxvv, vy.gxvv, vz.gxvv,
+                                          nx.gxvv, ny.gxvv, nz.gxvv,
+                                          color_vv.gxvv, gxg.C_GREY,
+                                          vf1.gxvv, vf2.gxvv, vf3.gxvv,
+                                          v3d.coordinate_system.gxipj)
